7c507202a0fa0df3e28f85efaf68448f43918825
france marmousez <fmarm@MacBook-Pro-de-france.local>
2018-11-01 11:47:24 +0100

    first commit

---
+++ code_housing.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 31 22:08:17 2018
+
+@author: fmarm
+"""
+
+import pandas as pd
+
+test = pd.read_csv("test.csv")
+train = pd.read_csv("train.csv")
+
